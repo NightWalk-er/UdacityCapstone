96b3ce8ff7d0b2567192ea17923fb0245f40a621
Nimishainori <56473988+Nimishainori@users.noreply.github.com>
2020-05-29 10:23:42 +0530

    code added

--- app.py
+++ app.py
@@ -17,50 +17,39 @@ def create_app(test_config=None):
       response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
       response.headers.add('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
       return response
-  
+
+
   @app.route('/movies')
   @requires_auth('get:movies')
   def get_movies(token):
-      movies = Movie.query.all()
-      movie_names = [movie.format() for movie in movies]
+    movies = Movie.query.all()
+    movie_names = [m.format() for m in movies]
 
-      if len(movie_names) == 0:
-        abort(404,{'message':'No movie found in database'})
+    if len(movie_names) == 0:
+      abort(404)
 
-      for movie in movie_names:
-        movie['actors'] = [i.format() for i in movie_names['actors']]
-      return jsonify({
+    for movie in movie_names:
+      movie['actors'] = [actor.format() for actor in movie['actors']]
+
+    message = {
         'success': True,
-        'movies': movie_names
-      })
-  
-  @app.route('/actors')
-  @requires_auth('get:actors')
-  def get_actors(token):
-      actors = Actor.query.all()
-      actor_names = [actor.format() for actor in actors]
-      
-      if len(actor_names) == 0:
-        abort(404,{'message':'No actor found in database'})
-      
-      return jsonify({
-        'success': True,
-        'Actors':actor_names
-      })
+        'movies': movie_names,
+        'total movies': len(movie_names)
+      }
+
+    return jsonify(message)
 
   @app.route('/movies/create', methods=['POST'])
   @requires_auth('post:movie')
   def post_new_movie(token):
-      body = request.get_json()
+    body = request.get_json()
 
-      if not body:
-        abort(400,{'message':'Require JSON body does not exist'})
-
+    try:
       title = body.get('title', None)
       release_date = body.get('release_date', None)
 
       if not title:
-        abort(422,{'message':'No title given'})
+        abort(422)
 
       movie = Movie(title=title, release_date=release_date)
       movie.insert()
@@ -72,23 +61,57 @@ def create_app(test_config=None):
         'created': movie.id,
         'new_movie': new_movie
       })
+    
+    except:
+      abort(400)
+
+  
+  @app.route('/movies/delete/<int:movie_id>', methods=['DELETE'])
+  @requires_auth('delete:movie')
+  def delete_movie(token, movie_id):
+    try:
+      del_movie = Movie.query.filter(Movie.id == movie_id).one_or_more()
+
+      if del_movie is None:
+        abort(404)
+
+      db.session.commit()
+      db.session.close()
+      return jsonify({
+        "success": True,
+        "movie id deletd": movie_id, # displaying id of deleted movie
+        "message" : "Delete occured",
+        "total movies": len(Movie.query.all())
+      })
+    except:
+      abort(422)
+
+
+  @app.route('/actors')
+  @requires_auth('get:actors')
+  def get_actors(token):
+    actors = Actor.query.all()
+    actor_names = [actor.format() for actor in actors]
+      
+    if len(actor_names) == 0:
+      abort(404)
+      
+    return jsonify({
+      'success': True,
+      'Actors':actor_names,
+      'Total actors': len(Actor.query.all())
+    })
 
   @app.route('/actors/create', methods=['POST'])
   @requires_auth('post:actor')
   def post_new_actor(token):
-      body = request.get_json()
-
-      if not body:
-        abort(400,{'message':'request does not contain a valid JSON body'})
-      
+    body = request.get_json()
+    try:
       name = body.get('name', None)
       age = body.get('age', None)
 
-      if not name:
-      abort(422, {'message': 'no name provided.'})
-
-      if not age:
-      abort(422, {'message': 'no age provided.'})
+      if not name or not age:
+        abort(422)
 
       gender = body.get('gender', None)
       movie_id = body.get('movie_id', None)
@@ -103,57 +126,36 @@ def create_app(test_config=None):
         'created': actor.id,
         'new_actor': new_actor
       })
-
-  @app.route('/movies/delete/<int:movie_id>', methods=['DELETE'])
-  @requires_auth('delete:movie')
-  def delete_movie(token, movie_id):
-
-      if not movie_id:
-        abort(400,{'message':'Append a movie id'})
-
-      movie_to_be_deleted=Movie.query.filter(Movie.id == movie_id).one_or_more()
-
-      if not movie_to_be_deleted:
-        abort(404,{'message':'id {} not found'.format(movie_id)})
-
-      db.session.commit()
-      db.session.close()
-      return jsonify({
-        "success": True,
-        "message" : "Delete occured"
-      })
+    except:
+      abort(400,{"message":"JSON body required"})
 
   @app.route('/actors/delete/<int:actor_id>', methods=['DELETE'])
   @requires_auth('delete:actor')
   def delete_actor(token, actor_id):
-
-      if not actor_id:
-        abort(400,{'message':'Append a actor id'})
-
-      actor_to_delete = Actor.query.filter(Actor.id == actor_id).one_or_more()
-      if not actor_to_delete:
-        abort(404,{'message':'id {} not found'.format(actor_id)})
+    try:
+      del_actor = Actor.query.filter(Actor.id == actor_id).one_or_more()
+      if del_actor is None:
+        abort(404)
 
       db.session.commit()
       db.session.close()
       return jsonify({
         "success": True,
-        "message" : "Delete occured"
+        "message" : "Delete occured",
+        "id" : actor_id, # displaying id of actor whose data is deleted 
+        "Total Actor Remaining": len(Actor.query.all())
       })
+    except:
+      abort(422)
 
   @app.route('/actors/patch/<int:actor_id>', methods=['PATCH'])
   @requires_auth('patch:actors')
-  def patch_actor(toekn, actor_id):
-      body = request.get_json()
-
-      if not actor_id:
-        abort(400,{'message':'Append an actor'})
-      if not body:
-        abort(400,{'message':'Does not have valid JSON body'})
-
+  def patch_actor(token, actor_id):
+    body = request.get_json()
+    try:
       actor = Actor.query.filter(Actor.id == actor_id)
       if not actor:
-        abort(404,{'message':'Actor with id {} not found in database'.format(actor_id)})
+        abort(404,{'message':'id {} not found'.format(actor_id)})
 
       name = body.get('name', None)
       age = body.get('age', None)
@@ -166,35 +168,15 @@ def create_app(test_config=None):
       actor.age = age
       actor.gender = gender
       actor.movie_id = movie_id
-
       actor.update()
 
       return jsonify({
         "success": True,
         "message": "update occured"
+        "total actors": len(Actor.query.all())
       })
-    
-  @app.route('/movies/patch/<int:movie_id>')
-  @requires_auth('patch:movies')
-  def patch_movie(token, movie_id):
-      
-      body = request.get_json()
-      if not body:
-        abort(400,{'message':'No JSON body associated'})
-
-      movie = Movie.query.filter(Movie.id == movie_id)
-      if not body:
-        abort(404,{'message':'Movie id {} not found'.format(movie_id)})
-      
-      title = body.get('title', None)
-      release_date = body.get('release_date', None)
-      movie.title = title
-      movie.release_date = release_date
-      movie.update()
-      return jsonify({
-        "success": True,
-        "message": "update occured"
-      })
+    except:
+      abort(400,{"message":"either body is missing or actor's id is misssing"})
       
   ## Error Handlers
 
